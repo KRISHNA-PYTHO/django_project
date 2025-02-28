c05deb6b67ef40d6d6861563a08e150accfff038
Krishna mourya <maurya1389@gmail.com>
2025-02-28 22:02:58 +0530

    update

--- LorealEcommerce/Ecommerce/views.py
+++ LorealEcommerce/Ecommerce/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from . models import Pet
 # Create your views here.
+def home(request):
+    return render(request, 'ecommerce/home.html')
+
+
+def about(request):
+    return render(request, 'ecommerce/about.html')
+
+
+def contact(request):
+    return render(request, 'ecommerce/contact.html')
+
+
+def dog_categories(request):
+    dc = Pet.objects.filter(category='DOG')
+    return render(request,'ecommerce/dog_categories.html',{'dc':dc})
+
+
+def cat_categories(request):
+    dc = Pet.objects.filter(category='CAT')
+    return render(request,'ecommerce/cat_categories.html',{'dc':dc})
+
+
+def bird_categories(request):
+    return render(request,'ecommerce/bird_categories.html')
+
+def pet_details(request,id):
+    dc= Pet.objects.get(pk=id)
+    return render(request,'ecommerce/pet_details.html',{'dc':dc})
